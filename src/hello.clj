36cbab867730b2e4dbf5059b2e91cd6f90eed41e
ofadeyi <ofadeyi@gmail.com>
2017-03-12 15:34:28 +0000

    LEARN-008 | Ola | Adding content negotiation via an interceptor

--- src/hello.clj
+++ src/hello.clj
@@ -1,9 +1,15 @@
 (ns hello
-  (:require [io.pedestal.http :as http]
+  (:require [clojure.string :as st]
+            [clojure.data.json :as json]
+            [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
-            [clojure.string :as st]))
+            [io.pedestal.http.content-negotiation :as conneg]))
 
 (def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
 
 (defn ok [body]
   {:status 200 :body body})
@@ -26,6 +32,7 @@
     (empty? query-name)         "Hello, world!\n"
     :else                       (str "Hello, " query-name "!\n")))
 
+;; Handlers
 (defn respond-hello [request]
   (let [qname (get-in request [:query-params :name])
         rresponse (greeting-for qname)]
@@ -33,17 +40,35 @@
       (ok rresponse)
       (not-found))))
 
+;; Interceptors
 (def echo
-  {
-   :name ::echo
+  {:name ::echo
    :enter (fn [context]
             (let [request (:request context)
                   response (ok request)]
               (assoc context :response response)))})
 
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+         (fn [context]
+           (let [accepted         (get-in context [:request :accept :field] "text/plain")
+                 response         (get context :response)
+                 body             (get response :body)
+                 coerced-body     (case accepted
+                                    "text/html"        body
+                                    "text/plain"       body
+                                    "application/edn"  (pr-str body)
+                                    "application/json" (json/write-str body))
+                 updated-response (assoc response
+                                    :headers {"Content-Type" accepted}
+                                    :body    coerced-body)]
+             (assoc context :response updated-response)))})
+
+;; Routes
 (def routes
   (route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
      ["/echo" :get echo]}))
 
 (defn create-server []
